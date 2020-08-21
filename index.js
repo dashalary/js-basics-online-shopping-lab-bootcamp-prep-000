adb4dfb91a00d4bf9de73491ed04cd7278e6e8e6
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-21 19:46:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function placeOrder(cardNumber) {
   const cartTotal = total();
   
   if (cardNumber === 12345678) {
- 
+ cart.length = 0;
   return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
 }
 }
