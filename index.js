7e476089e1ce611354e3732d1b12190a10265f92
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-21 19:24:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
 let i = 0;
 if (cart.itemName === item)
   for (let i = 0; i < cart.length; i++) {
-  cart.splice(, 1)
+  cart.splice(i, 1)
 }
 else { return `That item is not in your cart.`
 }
